d350dd67c417346b0acaf89de5ef64b3debcf673
Vedant Nandwana <vedantnn7.git@gmail.com>
2024-04-07 23:06:42 +0530

    implement basic lexer

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -2,24 +2,61 @@
 #define LEXER_H
 
 #include <stdio.h>
-#include "token.h"
 
-struct SLexer {
-    char* input;
+typedef enum EToken {
+    IDEN,
+    LPAREN,
+    RPAREN,
+    COLON,
+    SEMI,
+    ASSIGNMENT,
+    RETURN_TYPE_OPERATOR, // ::
+    BLOCK_TERMINATOR, // ;;
+
+    // error, some type like in rust
+    BANG,
+    OPTIONAL,
+
+    INT,
+    STRING,
+    FLOAT,
+
+    // math operators
+    DIVIDE,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+
+    END,
+} Token;
+
+typedef struct SToken {
+    Token type;
+    char* lexeme;
+    // unsigned short offset;
+} token_t;
+
+typedef struct SLexer {
+    const char* input;
     char ch;
     size_t length;
     size_t offset;
-};
+} lexer_t;
 
-typedef struct SLexer lexer_t;
+lexer_t *init_lexer(char* input);
 
-lexer_t *init_lexer(char *input);
+token_t* make_token(Token type, char* value);
 
-char peek(lexer_t *lexer, int offset);
+token_t* next(lexer_t* lexer);
 
-void skip_whitespace(lexer_t *lexer);
+void next_offset(lexer_t* lexer);
 
-void next(lexer_t *lexer);
+token_t* read_iden(lexer_t* lexer);
+
+void skip_whitespace(lexer_t* lexer);
+
+char peek(lexer_t *lexer);
+
 
 #endif
 
--- src/lexer.c
+++ src/lexer.c
@@ -1,32 +1,127 @@
 #include "include/lexer.h"
-#include "include/token.h"
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-lexer_t *init_lexer(char *input) {
-  lexer_t *lexer = malloc(sizeof(lexer_t));
-  lexer->input = input;
-  lexer->length = sizeof(input);
-  lexer->offset = 0;
-  lexer->ch = input[0];
+lexer_t *init_lexer(char* input) {
+    lexer_t *lexer = malloc(sizeof(lexer_t));
 
-  return lexer;
+    lexer->input = input;
+    lexer->length = sizeof(input);
+    lexer->offset = 0;
+    lexer->ch = input[0];
+
+    return lexer;
 }
 
-void skip_whitespace(lexer_t *lexer) {
-  if (lexer->ch == ' ' || lexer->ch == '\t' || lexer->ch == '\n' ||
-      lexer->ch == '\r') {
-    lexer->ch = lexer->input[lexer->offset++];
-    return;
-  }
-}
+token_t* make_token(Token type, char* value) {
+    token_t* token = malloc(sizeof(token_t));
 
-char peek(lexer_t *lexer, int offset) {
-    return lexer->input[offset++];
-}
+    token->type = type;
+    token->lexeme = value;
 
-token_t* tok_next(lexer_t *lexer) {
-    token_t* token = calloc(1, sizeof(token_t));
     return token;
 }
 
+void skip_whitespace(lexer_t *lexer) {
+  if (isspace(lexer->ch)) {
+    lexer->ch = lexer->input[lexer->offset++];
+  }
+}
+
+char peek(lexer_t *lexer) {
+    if (lexer->offset >= lexer->length) {
+        return '\0';
+    }
+
+    return lexer->input[lexer->offset + 1];
+}
+
+void next_offset(lexer_t *lexer) {
+    lexer->ch = lexer->input[lexer->offset++];     
+}
+
+token_t* read_iden(lexer_t* lexer) {
+    size_t start = lexer->offset;
+
+    while (isalpha(lexer->ch)) {
+        next_offset(lexer);
+    }
+        
+    char* lexeme = malloc((lexer->length - start) + 1);
+    strncpy(lexeme, lexer->input+start, lexer->offset); 
+
+    lexeme[sizeof(lexeme)] = '\0';
+
+    next_offset(lexer);
+    
+    return make_token(IDEN, lexeme);
+}
+
+token_t* next(lexer_t *lexer) {
+    skip_whitespace(lexer);
+
+    switch (lexer->ch) {
+        case '(':
+            return make_token(LPAREN, NULL);
+            break;
+
+        case ')':
+            return make_token(RPAREN, NULL);
+            break;
+
+        case '!':
+            return make_token(BANG, NULL); 
+            break;
+
+        case '?':
+            return make_token(OPTIONAL, NULL); 
+            break;
+
+        case '+':
+            return make_token(PLUS, NULL); 
+            break;
+
+        case '-':
+            return make_token(MINUS, NULL); 
+            break;
+
+        case '=':
+            return make_token(ASSIGNMENT, NULL); 
+            break;
+
+        case '/':
+            return make_token(DIVIDE, NULL); 
+            break;
+
+        case '*':
+            return make_token(MULTIPLY, NULL); 
+            break;
+
+        case ':':
+            if (peek(lexer) == ':') {
+                lexer->ch = lexer->input[lexer->offset++];
+                return make_token(RETURN_TYPE_OPERATOR, NULL); 
+                break;
+            }
+
+            return make_token(COLON, NULL);
+            break;
+
+        case ';':
+            if (peek(lexer) == ';') {
+                lexer->ch = lexer->input[lexer->offset++];
+                return make_token(BLOCK_TERMINATOR, NULL); 
+                break;
+            }
+
+            return make_token(SEMI, NULL);
+            break;
+
+        case '\0':
+            return make_token(END, NULL);
+    }
+
+}
 
