6c8bc64af0d15c1f2c30ef70f9a5ec723edc56b4
Vedant Nandwana <vedantnn7.git@gmail.com>
2024-04-08 21:42:25 +0530

    read args and print lexers

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "include/lexer.h"
 
-int main(void) {
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        fprintf(stderr, "usage: popsicle [options] [command]\n");
+        return 1; 
+    }
+
+    lexer_t *lexer = init_lexer(argv[1]);
+    read_next(lexer); 
+
+    if (lexer != NULL) free(lexer);
+
+    return 0;
 
 }
 
